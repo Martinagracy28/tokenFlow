369122f50c30096c2d6bd3b324e94d0c4bf2ee99
Martinagracy28 <martinagracy28@gmail.com>
2026-04-08 21:45:01 +0530

    I develop secure ERC20 token contracts with minting, burning, pausable transfers, and supply control using OpenZeppelin standards.

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,10 +3,4 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
-  networks: {
-    sepolia: {
-      url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
 };
